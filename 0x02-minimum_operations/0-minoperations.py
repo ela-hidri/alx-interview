b00dfac108ce78fd309aaba073c560a9b4d5e8cc
ela-hidri <elahidri.eh@gmail.com>
2024-01-16 12:04:17 -0800

    adding 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Solve Minimum Operations
+"""
+
+
+def minOperations(n):
+    """
+    calculates the fewest number of operations needed to result in exactly n
+    H characters in the file
+
+    Keyword arguments:
+    n -- n H characters in the file
+    Return: fewest number of operations or 0 if n is impossible
+    """
+    if n < 0:
+        return 0
+    divs = []
+    i = 2
+    while n != 1:
+        if n % i == 0:
+            n = n // i
+            divs.append(i)
+        else:
+            i += 1
+    return sum(divs)
