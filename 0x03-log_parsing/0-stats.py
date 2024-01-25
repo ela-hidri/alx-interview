fd4bf75bd095c6dcceca770856a62dcb33b41e48
ela-hidri <elahidri.eh@gmail.com>
2024-01-25 11:23:40 -0800

    fixing stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,11 +11,13 @@ def addCodes(code):
                 StatusCodes[nb] = (stCode, count + 1)
                 return
         StatusCodes.append((code, 1))
-pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-i = 0
-TotalFileSize = 0
-StatusCodes = []
+
 try:
+    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
+    i = 0
+    TotalFileSize = 0
+    StatusCodes = []
+
     for x in sys.stdin:
         i += 1
         match = re.match(pattern, x)
@@ -24,12 +26,13 @@ try:
             addCodes(match.group(3))
             TotalFileSize += int(FileSize)
 
-        if i == 10:
+        if i % 10 == 0:
             print(f'File size: {TotalFileSize}')
             for code, nb in sorted(StatusCodes):
                 print(f'{code}: {nb}')
-            i = 0
-except KeyboardInterrupt:
+except Exception as err:
+    pass
+finally:
     print(f'File size: {TotalFileSize}')
     for code, nb in sorted(StatusCodes):
         print(f'{code}: {nb}')
