3a4d042ba6e082c304fbf1191cddb5714f5d6257
ela-hidri <elahidri.eh@gmail.com>
2024-01-25 13:08:36 -0800

    fixing file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,30 +12,30 @@ def addCodes(code):
                 return
         StatusCodes.append((code, 1))
 
+if __name__ == "__main__":
+    codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    try:
+        pattern = (r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+                r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] '
+                r'"GET \/projects\/260 HTTP\/1\.1" (.{3}) (\d+)$')
+        i = 0
+        TotalFileSize = 0
+        StatusCodes = []
 
-codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-try:
-    pattern = (r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
-               r'\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] '
-               r'"GET \/projects\/260 HTTP\/1\.1" (.{3}) (\d+)$')
-    i = 0
-    TotalFileSize = 0
-    StatusCodes = []
+        for x in sys.stdin:
+            i += 1
+            match = re.match(pattern, x)
+            if (match):
+                FileSize = match.group(3)
+                if match.group(2) in codes:
+                    addCodes(match.group(2))
+                TotalFileSize += int(FileSize)
 
-    for x in sys.stdin:
-        i += 1
-        match = re.match(pattern, x)
-        if (match):
-            FileSize = match.group(3)
-            if match.group(2) in codes:
-                addCodes(match.group(2))
-            TotalFileSize += int(FileSize)
-
-        if i % 10 == 0:
-            print(f'File size: {TotalFileSize}')
-            for code, nb in sorted(StatusCodes):
-                print(f'{code}: {nb}')
-finally:
-    print(f'File size: {TotalFileSize}')
-    for code, nb in sorted(StatusCodes):
-        print(f'{code}: {nb}')
+            if i % 10 == 0:
+                print(f'File size: {TotalFileSize}')
+                for code, nb in sorted(StatusCodes):
+                    print(f'{code}: {nb}')
+    finally:
+        print(f'File size: {TotalFileSize}')
+        for code, nb in sorted(StatusCodes):
+            print(f'{code}: {nb}')
