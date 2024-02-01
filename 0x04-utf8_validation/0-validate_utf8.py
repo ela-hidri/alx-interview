03dafa2514b4913473e92b3cda02d4b84ef4d73e
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-01 21:22:30 +0100

    adding 0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+    determines if a given data set represents a valid UTF-8 encoding.
+    """
+    count = 0
+    for byte in data:
+        if count == 0:
+            if byte >> 3 == 0b11110:
+                count = 3
+            elif byte >> 4 == 0b1110:
+                count = 2
+            elif byte >> 5 == 0b110:
+                count = 1
+            elif byte >> 7 == 0b1:
+                return False
+        else:
+            if byte >> 6 == 0b10:
+                count -= 1
+            else:
+                return False
+    return count == 0
