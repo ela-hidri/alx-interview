de5e828a1e9ee37d054c5eb3b6c9286626849236
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-13 20:56:12 +0100

    adding changes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,36 @@
 #!/usr/bin/node
 const argv = process.argv;
 const request = require('request');
-async function Movies(){
-    await request('https://swapi-api.alx-tools.com/api/films/' + argv[2], function (error, response, body) {
+function Movies(){
+    request('https://swapi-api.alx-tools.com/api/films/' + argv[2], function (error, response, body) {
     const ch = JSON.parse(body).characters;
-    ch.forEach(c => {
-        request(c, function (error, response, body) {
-        const name = JSON.parse(body).name;
-        console.log(name);
-        if (error) {
-            console.error(error);
+    ch.forEach(async url => {
+        try {
+            const data = await makeRequest(url);
+            console.log(data); // Do something with the data
+        } catch (error) {
+            console.error('Error fetching data:', error);
         }
-        });
     });
     if (error) {
         console.error(error);
     }
+    });
+}
+
+function makeRequest(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (error) {
+                reject(error);
+            } else {
+                const name = JSON.parse(body).name;
+                console.log(name);
+                if (error) {
+                    console.error(error);
+                }
+            }
+        });
     });
 }
 
