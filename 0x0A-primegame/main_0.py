e9a21bb95f3d037d61e2ded23a6210d4187988ab
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-03-14 23:31:15 +0100

    updating 0-prime_game

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -10,3 +10,5 @@ print(isWinner(5, [2, 3, 4, 5, 6]))  # Output should be "Maria"
 print(isWinner(4, [11, 13, 17, 19]))  # Output should be "Ben"
 print(isWinner(2, [7, 8]))  # Output should be "Maria"
 print(isWinner(1, [20]))  # Output should be None
+print("Winner: {}".format(isWinner(0, [0])))
+print("Winner: {}".format(isWinner(-1, [10])))
